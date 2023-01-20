3a9c80b7e088f1207396682f28c70e5bfb9a6baa
prakashdeep2902 <prakashdeep2902@gmail.com>
2023-01-20 10:54:12 +0530

    view details commited

--- src/component/WeekDetials.js
+++ src/component/WeekDetials.js
@@ -1,10 +1,158 @@
 import React from 'react'
-
+import '../App.css'
 const WeekDetials = () => {
   return (
-    <div>
-      <h1>this is per week details</h1>
-    </div>
+    <>
+
+      <div className='container_habitdetails'>
+
+        <div className='habitdetails_title'>
+          <p>reading</p>
+        </div>
+        <div className='all_week_days'>
+          <div className='day'>
+            <p>sunday</p>
+          </div>
+          <div className='date'>
+            <p>29/02/2023</p>
+          </div>
+          <div className='status'>
+            <div className='sign'>
+              <i class="fa-solid fa-check"></i>
+            </div>
+            <div className='sign'>
+              <i class="fa-solid fa-xmark"></i>
+            </div>
+            <div className='sign'>
+              <i class="fa-solid fa-minus"></i>
+            </div>
+          </div>
+          
+        </div>
+        <div className='all_week_days'>
+          <div className='day'>
+            <p>monday</p>
+          </div>
+          <div className='date'>
+            <p>29/02/2023</p>
+          </div>
+          <div className='status'>
+            <div className='sign'>
+              <i class="fa-solid fa-check"></i>
+            </div>
+            <div className='sign'>
+              <i class="fa-solid fa-xmark"></i>
+            </div>
+            <div className='sign'>
+              <i class="fa-solid fa-minus"></i>
+            </div>
+          </div>
+          
+        </div>
+        <div className='all_week_days'>
+          <div className='day'>
+            <p>tuesday</p>
+          </div>
+          <div className='date'>
+            <p>29/02/2023</p>
+          </div>
+          <div className='status'>
+            <div className='sign'>
+              <i class="fa-solid fa-check"></i>
+            </div>
+            <div className='sign'>
+              <i class="fa-solid fa-xmark"></i>
+            </div>
+            <div className='sign'>
+              <i class="fa-solid fa-minus"></i>
+            </div>
+          </div>
+          
+        </div>
+        <div className='all_week_days'>
+          <div className='day'>
+            <p>wednesday</p>
+          </div>
+          <div className='date'>
+            <p>29/02/2023</p>
+          </div>
+          <div className='status'>
+            <div className='sign'>
+              <i class="fa-solid fa-check"></i>
+            </div>
+            <div className='sign'>
+              <i class="fa-solid fa-xmark"></i>
+            </div>
+            <div className='sign'>
+              <i class="fa-solid fa-minus"></i>
+            </div>
+          </div>
+          
+        </div>
+        <div className='all_week_days'>
+          <div className='day'>
+            <p>thursday</p>
+          </div>
+          <div className='date'>
+            <p>29/02/2023</p>
+          </div>
+          <div className='status'>
+            <div className='sign'>
+              <i class="fa-solid fa-check"></i>
+            </div>
+            <div className='sign'>
+              <i class="fa-solid fa-xmark"></i>
+            </div>
+            <div className='sign'>
+              <i class="fa-solid fa-minus"></i>
+            </div>
+          </div>
+          
+        </div>
+        <div className='all_week_days'>
+          <div className='day'>
+            <p>friday</p>
+          </div>
+          <div className='date'>
+            <p>29/02/2023</p>
+          </div>
+          <div className='status'>
+            <div className='sign'>
+              <i class="fa-solid fa-check"></i>
+            </div>
+            <div className='sign'>
+              <i class="fa-solid fa-xmark"></i>
+            </div>
+            <div className='sign'>
+              <i class="fa-solid fa-minus"></i>
+            </div>
+          </div>
+          
+        </div>
+        <div className='all_week_days'>
+          <div className='day'>
+            <p>saturday</p>
+          </div>
+          <div className='date'>
+            <p>29/02/2023</p>
+          </div>
+          <div className='status'>
+            <div className='sign'>
+              <i class="fa-solid fa-check"></i>
+            </div>
+            <div className='sign'>
+              <i class="fa-solid fa-xmark"></i>
+            </div>
+            <div className='sign'>
+              <i class="fa-solid fa-minus"></i>
+            </div>
+          </div>
+          
+        </div>
+
+      </div>
+
+    </>
   )
 }
 
